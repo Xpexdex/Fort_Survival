27fc8e281461674aea5bf411fedee33ff911b18d
Xpexdex <73250542+Xpexdex@users.noreply.github.com>
2021-02-09 11:44:28 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
 function setup() {
   createCanvas(screen.width,screen.height);
-}
+};
 
 function draw() {
   background(0);
-}
+};
+var Area = Login;
+var X = 0;
+var Y = 0;
+var Z = 0;
+var Items = [false,false,false,false];
+
